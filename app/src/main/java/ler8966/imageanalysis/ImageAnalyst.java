1f8a7752ebfe2099f24166c496dd07a6ba059c4c
Tamara Tershakovec <t42itoz@gmail.com>
2016-01-11 10:17:42 -0500

    added find RED methods

--- app/src/main/java/ler8966/imageanalysis/ImageAnalyst.java
+++ app/src/main/java/ler8966/imageanalysis/ImageAnalyst.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 
 public class ImageAnalyst {
     private Bitmap bmp;
@@ -45,6 +46,76 @@ public class ImageAnalyst {
             temp[i] = c;
         }
         return temp;
+    }
+
+    public int[] showFindRed() {
+        int[] colors = ColorUtil.getColorArray(bmp);
+        int[] temp = new int[colors.length];
+        for (int i = 0; i < colors.length; i++) {
+            int b = colors[i];
+            if (isRed(b)) {
+                temp[i] = Color.rgb(255, 0, 0);
+            } else {
+                temp[i] = Color.rgb(0, 0, 0);
+            }
+        }
+        return temp;
+    }
+
+    public int[] somewhatSimpleRedDetection() {
+        int[] distribution = new int[bmp.getWidth()];
+        for (int i = 0; i < bmp.getHeight(); i++) {
+            for (int j = 0; j < bmp.getWidth(); j++) {
+                int b = bmp.getPixel(j, i);
+                if (isRed(b)) {
+                    distribution[j]++;
+                }
+            }
+        }
+        Log.d("ImageAnalyst", Arrays.toString(distribution));
+        return null;
+    }
+
+    public int superSimpleRedDetection() {
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < bmp.getHeight(); i++) {
+            for (int j = 0; j < bmp.getWidth() / 2; j++) {
+                int b = bmp.getPixel(j, i);
+                if (isRed(b)) {
+                    left++;
+                }
+            }
+        }
+
+        for (int i = 0; i < bmp.getHeight(); i++) {
+            for (int j = bmp.getWidth() / 2; j < bmp.getWidth(); j++) {
+                int b = bmp.getPixel(j, i);
+                if (isRed(b)) {
+                    right++;
+                }
+            }
+        }
+        Log.d("ImageAnalyst", "right: " + right);
+        Log.d("ImageAnalyst", "left: " + left);
+        return right - left;
+    }
+
+
+    // todo - implement threshold?
+    int threshold = 70; // lower is looser (more will show up as red)
+
+    public boolean isRed(int b) {
+        int blue = Color.blue(b);
+        int red = Color.red(b);
+        int green = Color.green(b);
+        int blueGreenAvg = (blue + green) / 2;
+        if ((red - blueGreenAvg) > threshold) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public String getReportMessage() {
