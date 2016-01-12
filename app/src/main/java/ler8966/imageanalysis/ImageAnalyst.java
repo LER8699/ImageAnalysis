8dde42db82b7171333daf40cb3487c507702ce9e
Tamara Tershakovec <t42itoz@gmail.com>
2016-01-12 08:49:37 -0500

    moved some methods to ImageAlterer

--- app/src/main/java/ler8966/imageanalysis/ImageAnalyst.java
+++ app/src/main/java/ler8966/imageanalysis/ImageAnalyst.java
@@ -22,46 +22,6 @@ public class ImageAnalyst {
         return bmp;
     }
 
-    public void analyzePNGBytes() {
-        byte[] bytes = getBytes();
-
-        Log.d("ImageAnalyst", "byteArray length: " + bytes.length);
-        Log.d("ImageAnalyst", "bitmap width: " + bmp.getWidth());
-    }
-
-    private byte[] getBytes() {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        return stream.toByteArray();
-    }
-
-    public int[] swapRedAndBlue() {
-        int[] colors = ColorUtil.getColorArray(bmp);
-        int[] temp = new int[colors.length];
-        for (int i = 0; i < colors.length; i++) {
-            int b = colors[i];
-            int blue = Color.blue(b);
-            int red = Color.red(b);
-            int c = Color.rgb(blue, Color.green(b), red); // swap red & blue
-            temp[i] = c;
-        }
-        return temp;
-    }
-
-    public int[] showFindRed() {
-        int[] colors = ColorUtil.getColorArray(bmp);
-        int[] temp = new int[colors.length];
-        for (int i = 0; i < colors.length; i++) {
-            int b = colors[i];
-            if (isRed(b)) {
-                temp[i] = Color.rgb(255, 0, 0);
-            } else {
-                temp[i] = Color.rgb(0, 0, 0);
-            }
-        }
-        return temp;
-    }
-
     /**
      * takes distribution of a color and calculates location
      * For example: array [0,0,0,1,1,2,2,3,4,10,20,22,34,45] represents a high concentration of
@@ -70,23 +30,27 @@ public class ImageAnalyst {
      * This method takes the middle of the distribution and returns how much to the right or left
      * of the middle the blob is (negative for left, positive for right). It is not weighted as to
      * the values - maybe later.
-     *
+     * <p/>
      * todo - if no red found, it returns center, which is wrong. Need a way to show no red found
      */
+
     public int analyzeDistribution(int[] distribution) {
-        int average = 0;
+      /*  int average = 0;
         for (int i = 0; i < distribution.length; i++) {
             average += distribution[i];
         }
         average = average / distribution.length;
-
+        Log.d("ImageAnalyst", "average = " + average);
         // normalize distribution by subtracting average (but not allowing negative numbers. If
         // subtracting average leads to a negative number, then use 0.
         for (int i = 0; i < distribution.length; i++) {
             int val = distribution[i] - average;
+           // Log.d("ImageAnalyst", "changing from " + distribution[i] + " to " + val);
             distribution[i] = val < 0 ? 0 : val;
         }
-
+        Log.d("ImageAnalyst", "Normalized Distribution: ");
+        Log.d("ImageAnalyst", Arrays.toString(distribution));
+        */
         // find first index of red
         int startIndex = 0;
         for (int i = 0; i < distribution.length; i++) {
@@ -104,7 +68,7 @@ public class ImageAnalyst {
             }
         }
 
-        Log.d("ImageAnalyst", "start/end = " + startIndex + ", " + endIndex);
+        Log.d("ImageAnalyst", "start/end = " + startIndex + " / " + endIndex);
         // average start and end indexes
         int blobMiddle = (startIndex + endIndex) / 2;
 
@@ -117,28 +81,46 @@ public class ImageAnalyst {
         return dirAmt;
     }
 
-    public int[] getVerticalRedDistribution() {
+    public int[] getVerticalDistribution(char rb) throws IllegalArgumentException {
         int[] distribution = new int[bmp.getWidth()];
         for (int i = 0; i < bmp.getHeight(); i++) {
             for (int j = 0; j < bmp.getWidth(); j++) {
                 int b = bmp.getPixel(j, i);
-                if (isRed(b)) {
-                    distribution[j]++;
+                if (rb == 'R') {
+                    if (ColorUtil.isRed(b)) {
+                        distribution[j]++;
+                    }
+                } else if (rb == 'B') {
+                    if (ColorUtil.isBlue(b)) {
+                        distribution[j]++;
+                    }
+                } else {
+                    throw new IllegalArgumentException(rb + " is not a valid argument - use R for red or B for blue.");
                 }
             }
         }
+
         Log.d("ImageAnalyst", Arrays.toString(distribution));
         return distribution;
     }
 
     public int superSimpleRedDetection() {
+        return superSimpleColorDetection('R');
+    }
+
+
+    public int superSimpleBlueDetection() {
+        return superSimpleColorDetection('B');
+    }
+
+    protected int superSimpleColorDetection(char rb) throws IllegalArgumentException {
         int left = 0;
         int right = 0;
 
         for (int i = 0; i < bmp.getHeight(); i++) {
             for (int j = 0; j < bmp.getWidth() / 2; j++) {
                 int b = bmp.getPixel(j, i);
-                if (isRed(b)) {
+                if (ColorUtil.isRed(b)) {
                     left++;
                 }
             }
@@ -147,8 +129,16 @@ public class ImageAnalyst {
         for (int i = 0; i < bmp.getHeight(); i++) {
             for (int j = bmp.getWidth() / 2; j < bmp.getWidth(); j++) {
                 int b = bmp.getPixel(j, i);
-                if (isRed(b)) {
-                    right++;
+                if (rb == 'R') {
+                    if (ColorUtil.isRed(b)) {
+                        right++;
+                    }
+                } else if (rb == 'B') {
+                    if (ColorUtil.isBlue(b)) {
+                        right++;
+                    }
+                } else {
+                    throw new IllegalArgumentException(rb + " is not a valid argument - use R for red or B for blue.");
                 }
             }
         }
@@ -159,19 +149,7 @@ public class ImageAnalyst {
 
 
     // todo - implement threshold?
-    int threshold = 70; // lower is looser (more will show up as red)
 
-    public boolean isRed(int b) {
-        int blue = Color.blue(b);
-        int red = Color.red(b);
-        int green = Color.green(b);
-        int blueGreenAvg = (blue + green) / 2;
-        if ((red - blueGreenAvg) > threshold) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     public String getReportMessage() {
         if (bmp == null) {
